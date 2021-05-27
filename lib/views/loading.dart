0280432337eef6822e873e3a00f60d3152fd705e
Marco Rapino <aktasway@gmail.com>
2021-05-27 16:10:24 +0200

    - decorated loading screen

--- lib/views/loading.dart
+++ lib/views/loading.dart
@@ -1,8 +1,10 @@
+import 'package:astropills_tools/core/theme.colors.dart';
 import 'package:astropills_tools/services/location.service.dart';
 import 'package:astropills_tools/services/moon.service.dart';
 import 'package:astropills_tools/services/weather.service.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading();
@@ -36,12 +38,14 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      backgroundColor: ThemeColors.primaryColorDark,
+      body: Container(
         child: Center(
-          child: Text(
-            'Loading...'
+          child: SpinKitRotatingCircle(
+            color: ThemeColors.interactiveColor,
+            size: 50.0,
           ),
-        )
+        ),
       )
     );
   }
